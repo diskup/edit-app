90bf8be79afc076d7eb686486300d1c48691d4c1
“diskup” <taigaa.1998@gmail.com>
2021-11-04 13:38:30 +0000

    編集2

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,9 +2,12 @@ class BlogsController < ApplicationController
 
   def index
     @blogs = Blog.all
+    p @blogs
   end
 
   def show
+    @blog = Blog.find(params[:id])
+
   end
 
   def new
@@ -12,13 +15,31 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog)
+    else
+      render :new
+    end
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blog_path(@blog)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path  end
 
   private
     def blog_params
